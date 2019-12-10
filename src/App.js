9cc3619fa10d7910c5c506148ee10311ff454bef
sathishkumar <sathishmani59@gmail.com>
2019-12-10 21:12:43 +0530

    created react project with creact react app and added files for homepage

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Homepage from'./components/Homepage.component';
+function App () {
+  return(
+    <div>
+      <Homepage > 
+          </Homepage>  
+    </div>
+  );
+}
+  
+
+export default App; 
