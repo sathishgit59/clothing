dac1381871cb91acfee3b252d5f251fd8182ed5c
sathishkumar <sathishmani59@gmail.com>
2019-12-10 23:20:47 +0530

    finished styling in menu-item component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Homepage from'./components/Homepage.component';
+import Homepage from'./pages/Homepage.component';
 function App () {
   return(
     <div>
